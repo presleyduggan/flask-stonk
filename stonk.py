e4c4d86ed53c4178cede1484d844380bcb58163e
presley <57386908+presleyduggan@users.noreply.github.com>
2021-01-18 23:10:07 -0800

    v1.0 --- made more mobile friendly (at the expensive of desktop - will maybe fix later), added footer, and cleaned up python

--- stonk.py
+++ stonk.py
@@ -12,51 +12,47 @@ from decimal import Decimal
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
-def printrand():
-    ticker = ['SNDL', 'PSTH', 'TSLA', 'SQ', 'IPOE', 'AJAX', 'F']
-    names = ['David', 'Poles', 'Presley', 'Mark', 'Jack', 'Rex', 'Jawsh']
-    startPrice = [0.68, 27.75, 867.92, 241.99, 19.02, 11.52, 9.00]
+def stonk_showcase():
+    myDict = {}
+
+    myDict["names"] = ['David', 'Jack', 'Jawsh', 'Mark', 'Poles', 'Presley', 'Rex']
+    myDict["ticker"] = ['SNDL', 'IPOE', 'F', 'SQ', 'PSTH', 'TSLA', 'AJAX']
+    myDict["startPrice"] = [0.68, 19.02, 9.00, 241.99, 27.75, 867.92, 11.52]
+
     currentPrice = []
     percentReturn = []
-    for elem in range(0,len(ticker)):
-        currentPrice.append(round(si.get_live_price(ticker[elem]),2))
 
-    for i in range(0, len(startPrice)):
-        percentReturn.append(round((((currentPrice[i]-startPrice[i])/startPrice[i])*100),2))
+    for elem in range(0,len(myDict["ticker"])):
+        currentPrice.append(round(si.get_live_price(myDict["ticker"][elem]),2))
+
+    for i in range(0, len(myDict["startPrice"])):
+        percentReturn.append(round((((currentPrice[i]-myDict["startPrice"][i])/myDict["startPrice"][i])*100),2))
 
     max_index = percentReturn.index(max(percentReturn))
     #print(max_index)
 
     #save values into dictionary for easy access in html
-    myDict = {}
-    myDict["names"] = names
-    myDict["ticker"] = ticker
-    myDict["startPrice"] = startPrice
     myDict["currentPrice"] = currentPrice
     myDict["percentReturn"] = percentReturn
     myDict["max"] = max_index
-    myDict["test"] = {False, False, False, False, True, False, False}
-    #test
 
     positive = []
     negative = []
 
-    for i in range(0, len(names)):
+    for i in range(0, len(myDict["names"])):
         negative.append(percentReturn[i] < 0)
         positive.append(percentReturn[i] >= 0)
-        # plt.bar(names[i][negative[i]], percentReturn[i][negative[i]], color = 'red')
-        # plt.bar(names[i][positive[i]], percentReturn[i][positive[i]], color = 'green')
 
 
-    for i in range(0, len(names)):
-        plt.bar(names[i], percentReturn[i][positive[i]], color = 'forestgreen', edgecolor = 'blue', linewidth = 1.5)
-        plt.bar(names[i], percentReturn[i][negative[i]], color = 'red', edgecolor = 'blue', linewidth = 1.5)
+    for i in range(0, len(myDict["names"])):
+        plt.bar(myDict["names"][i], percentReturn[i][positive[i]], color = 'forestgreen', edgecolor = 'blue', linewidth = 1.5)
+        plt.bar(myDict["names"][i], percentReturn[i][negative[i]], color = 'red', edgecolor = 'blue', linewidth = 1.5)
     
-    plt.ylabel('% Returnz', fontsize = 16, fontweight='bold')
+    plt.ylabel('% Return', fontsize = 16, fontweight='bold')
     plt.rc('axes', axisbelow=True)
     #plt.grid(False, color = 'gray', linestyle = 'dashed')
     plt.xlabel('Stonker', fontsize = 16, fontweight='bold')
-    plt.title('Current Returns: Stonks Competition', fontweight='bold', fontsize = 20)
+    plt.title('Current Returns', fontweight='bold', fontsize = 20)
     plt.savefig('static/stonk_graph.png', facecolor='blanchedalmond', edgecolor='none')
     return render_template('web.html', content= myDict)
 
