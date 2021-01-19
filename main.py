7596d1e357c66b538c3bbd14f84232cfc5c18a10
presley <57386908+presleyduggan@users.noreply.github.com>
2021-01-18 23:27:46 -0800

    update main.py

--- main.py
+++ main.py
@@ -72,4 +72,5 @@ def add_header(r):
 
 if __name__ == '__main__':
 	#app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run(host='0.0.0.0', debug= True)
+    #app.run(host='0.0.0.0', debug= True)
+    app.run()
